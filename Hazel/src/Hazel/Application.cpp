7f460799141a13e7c81e59ff45de0491c7c56b75
liujian <1508601494@qq.com>
2022-12-10 19:12:35 +0800

    Added event system + basic test.

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,4 +1,6 @@
 #include "Application.h"
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Log.h"
 
 namespace Hazel {
 	Application::Application() {
@@ -10,6 +12,10 @@ namespace Hazel {
 	}
 
 	void Application::Run() {
+
+		WindowResizeEvent e(1280, 720);
+		HZ_TRACE(e);
+
 		while (true) {
 
 		}
---
+++ Hazel/src/Hazel/Events/KeyEvent.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <sstream>
+
+#include "Event.h"
+
+namespace Hazel {
+	class HAZEL_API KeyEvent : public Event {
+	public:
+		inline int GetKeyCode() const { return m_KeyCode; }
+
+		EVENT_CLASS_CATEGORY(EventCategoryKeyboard | EventCategoryInput);
+
+	protected:
+		KeyEvent(int keycode)
+			:m_KeyCode(keycode) {
+		}
+
+		int m_KeyCode;
+	};
+
+
+	class HAZEL_API KeyPressEvent : public KeyEvent {
+	public:
+		KeyPressEvent(int keycode, int repeatCount)
+			: KeyEvent(keycode), m_RepeatCount(repeatCount) {
+		}
+
+		inline int GetRepeatCount() const { return m_RepeatCount; }
+
+		std::string ToString() const override {
+			std::stringstream ss;
+			ss << "KeyPressEvent: " << m_KeyCode << " (" << m_RepeatCount << " repeats)";
+			return ss.str();
+		}
+
+		/*	EVENT_CLASS_TYPE(KeyPressed) equal the follow three lines:
+				static EventType GetStaticType() { return EventType::KeyPressed; }
+				virtual EventType GetEventType() const override { return GetStaticType(); }
+				virtual const char* GetName() const override { return "KeyPressed"; }
+
+			事件类型永远是该类的事件，所以有一个静态成员就可以了。
+			可能会用到多态，所以需要虚函数 GetEventType() GetName()。
+		*/
+		EVENT_CLASS_TYPE(KeyPressed);
+
+
+	private:
+		int m_RepeatCount;
+	};
+
+
+	class HAZEL_API KeyReleasedEvent : public KeyEvent {
+	public:
+		KeyReleasedEvent(int keycode)
+			: KeyEvent(keycode) {
+		}
+
+		std::string ToString() const override {
+			std::stringstream ss;
+			ss << "KeyReleasedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyReleased)
+	};
+}
