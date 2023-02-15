6d619cf378b62bba503cbc6619a7a36c97e17e34
LiuJian <1508601494@qq.com>
2023-02-15 11:15:26 +0800

    Refresh gitignore.

--- Hazel/src/Hazel/ImGui/ImGuiLayer.h
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.h
@@ -1,11 +1,20 @@
 #pragma once
 
 #include "Hazel/Layer.h"
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Events/Event.h"
+#include "Hazel/Events/KeyEvent.h"
+#include "Hazel/Events/MouseEvent.h"
+
 
 namespace Hazel {
 	class HAZEL_API ImGuiLayer : public Layer {
 	private:
 		float m_Time = 0.0f;
+
+	private:
+
+
 	public:
 		ImGuiLayer();
 		~ImGuiLayer();
