fda5c7b72838bb8800482e7864baedf9fb9e20c1
LiuJian <1508601494@qq.com>
2023-03-22 22:36:14 +0800

    Added Hazel::Scope and Hazel::Ref definitions.

--- Hazel/src/Platform/OpenGL/OpenGLVertexArray.h
+++ Hazel/src/Platform/OpenGL/OpenGLVertexArray.h
@@ -11,14 +11,14 @@ namespace Hazel {
 		virtual void Bind() const override;
 		virtual void Unbind() const override;
 
-		virtual void AddVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuffer) override;
-		virtual void SetIndexBuffer(const std::shared_ptr<IndexBuffer>& indexBuffer) override;
+		virtual void AddVertexBuffer(const Ref<VertexBuffer>& vertexBuffer) override;
+		virtual void SetIndexBuffer(const Ref<IndexBuffer>& indexBuffer) override;
 
-		virtual const std::vector<std::shared_ptr<VertexBuffer>>& GetVertexBuffers() const override { return m_Vertexbuffers; }
-		virtual const std::shared_ptr<IndexBuffer>& GetIndexBuffer() const override  { return m_Indexbuffer; }
+		virtual const std::vector<Ref<VertexBuffer>>& GetVertexBuffers() const override { return m_Vertexbuffers; }
+		virtual const Ref<IndexBuffer>& GetIndexBuffer() const override  { return m_Indexbuffer; }
 	private:
 		uint32_t m_RendererID;
-		std::vector<std::shared_ptr<VertexBuffer> > m_Vertexbuffers;
-		std::shared_ptr<IndexBuffer> m_Indexbuffer;
+		std::vector<Ref<VertexBuffer> > m_Vertexbuffers;
+		Ref<IndexBuffer> m_Indexbuffer;
 	};
 }
