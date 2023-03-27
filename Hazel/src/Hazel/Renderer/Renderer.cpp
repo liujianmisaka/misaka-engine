f89cf8d34c77140e87c955f46d07479144910849
LiuJian <1508601494@qq.com>
2023-03-27 16:09:13 +0800

    Add Renderer2D and Upgrade gitignore

--- Hazel/src/Hazel/Renderer/Renderer.cpp
+++ Hazel/src/Hazel/Renderer/Renderer.cpp
@@ -2,6 +2,7 @@
 #include "Renderer.h"
 
 #include "Platform/OpenGL/OpenGLShader.h"
+#include "Renderer2D.h"
 
 namespace Hazel {
 
@@ -9,6 +10,7 @@ namespace Hazel {
 
 	void Renderer::Init() {
 		RenderCommand::Init();
+		Renderer2D::Init();
 	}
 
 	void Renderer::onWindowReSize(uint32_t width, uint32_t height) {
