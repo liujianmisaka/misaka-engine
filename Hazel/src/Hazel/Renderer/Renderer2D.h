5a2ce3b55f72a6b8b71a07b926c29c72bbb2a69d
liujian <liujianmisaka@qq.com>
2023-05-21 17:06:19 +0800

    Update Render2D files.
    Missing batch render leads to some render errors.

--- Hazel/src/Hazel/Renderer/Renderer2D.h
+++ Hazel/src/Hazel/Renderer/Renderer2D.h
@@ -1,53 +1,51 @@
 #pragma once
 
-#include "OrthographicCamera.h"
-#include "Texture.h"
-#include "SubTexture2D.h"
-#include "Camera.h"
+#include "Hazel/Renderer/OrthographicCamera.h"
+
+#include "Hazel/Renderer/Texture.h"
+
+#include "Hazel/Renderer/Camera.h"
 
 namespace Hazel {
-	class Renderer2D {
-	public:
-		static void Init();
-		static void Shutdown();
+
+    class Renderer2D {
+    public:
+        static void Init();
+        static void Shutdown();
 
         static void BeginScene(const Camera& camera, const glm::mat4& transform);
-        static void BeginScene(const OrthographicCamera& camera);  // TODO: Remove
-		static void EndScene();
-		static void Flush();
+        static void BeginScene(const OrthographicCamera& camera); // TODO: Remove
+        static void EndScene();
+        static void Flush();
 
-		// Primitives
-		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color);
-		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color);
-		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
-		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
-		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<SubTexture2D>& subtexture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
-        static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<SubTexture2D>& subtexture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
+        // Primitives
+        static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color);
+        static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color);
+        static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
+        static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
 
         static void DrawQuad(const glm::mat4& transform, const glm::vec4& color);
         static void DrawQuad(const glm::mat4& transform, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
 
-        // Rotation is in radians
-		static void DrawRotateQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const glm::vec4& color);
-		static void DrawRotateQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const glm::vec4& color);
-		static void DrawRotateQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
-		static void DrawRotateQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
-        static void DrawRotateQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const Ref<SubTexture2D>& subtexture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
-		static void DrawRotateQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const Ref<SubTexture2D>& subtexture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
+        static void DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const glm::vec4& color);
+        static void DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const glm::vec4& color);
+        static void DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
+        static void DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
 
         // Stats
         struct Statistics {
             uint32_t DrawCalls = 0;
             uint32_t QuadCount = 0;
 
-            uint32_t GetTotalVertexCount() { return QuadCount * 4; }
-            uint32_t GetTotalIndexCount() { return QuadCount * 6; }
+            uint32_t GetTotalVertexCount() const { return QuadCount * 4; }
+            uint32_t GetTotalIndexCount() const { return QuadCount * 6; }
         };
-
         static void ResetStats();
         static Statistics GetStats();
 
     private:
-        static void FlushAndReset();
-	};
+        static void StartBatch();
+        static void NextBatch();
+    };
+
 }
