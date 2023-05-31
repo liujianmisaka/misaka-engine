90a2c2ef66490195c67db6c8968c8e2723c0c517
liujian <liujianmisaka@qq.com>
2023-05-31 17:56:16 +0800

    Added missing 'SetFloat2' in Renderer for completeness.

--- Hazel/src/Platform/OpenGL/OpenGLShader.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLShader.cpp
@@ -191,7 +191,13 @@ namespace Hazel {
 		UploadUniformFloat(name, value);
 	}
 
-	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value) {
+    void OpenGLShader::SetFloat2(const std::string& name, const glm::vec2& value) {
+        HZ_PROFILE_FUNCTION();
+
+        UploadUniformFloat2(name, value);
+	}
+
+    void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value) {
 		HZ_PROFILE_FUNCTION();
 		
 		UploadUniformFloat3(name, value);
