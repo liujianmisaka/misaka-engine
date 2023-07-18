d7c6b42255890c48078f9e3b753b46f5d812cd6d
liujian <liujianmisaka@qq.com>
2023-07-18 21:04:28 +0800

    Added circle colliders.

--- Hazel/src/Hazel/Scene/Scene.cpp
+++ Hazel/src/Hazel/Scene/Scene.cpp
@@ -6,6 +6,7 @@
 #include <box2d/b2_body.h>
 #include <box2d/b2_fixture.h>
 #include <box2d/b2_polygon_shape.h>
+#include <box2d/b2_circle_shape.h>
 
 #include "Components.h"
 #include "Hazel/Renderer/Renderer2D.h"
@@ -75,6 +76,7 @@ namespace Hazel {
         CopyComponent<NativeScriptComponent>(dstSceneRegistry, srcSceneRegistry, enttMap);
         CopyComponent<Rigidbody2DComponent>(dstSceneRegistry, srcSceneRegistry, enttMap);
         CopyComponent<BoxCollider2DComponent>(dstSceneRegistry, srcSceneRegistry, enttMap);
+        CopyComponent<CircleCollider2DComponent>(dstSceneRegistry, srcSceneRegistry, enttMap);
 
         return  newScene;
     }
@@ -126,6 +128,22 @@ namespace Hazel {
                 fixtureDef.friction = bc2d.Friction;
                 fixtureDef.restitution = bc2d.Restitution;
                 fixtureDef.restitutionThreshold = bc2d.RestitutionThreshold;
+                body->CreateFixture(&fixtureDef);
+            }
+
+            if(entity.HasComponent<CircleCollider2DComponent>()) {
+                auto& cc2d = entity.GetComponent<CircleCollider2DComponent>();
+
+                b2CircleShape circleShape;
+                circleShape.m_p.Set(cc2d.Offset.x, cc2d.Offset.y);
+                circleShape.m_radius = cc2d.Radius;
+
+                b2FixtureDef fixtureDef;
+                fixtureDef.shape = &circleShape;
+                fixtureDef.density = cc2d.Density;
+                fixtureDef.friction = cc2d.Friction;
+                fixtureDef.restitution = cc2d.Restitution;
+                fixtureDef.restitutionThreshold = cc2d.RestitutionThreshold;
                 body->CreateFixture(&fixtureDef);
             }
         }
@@ -224,8 +242,8 @@ namespace Hazel {
             const auto group = m_Registry.group<TransformComponent>(entt::get<SpriteRendererComponent>);
             for (const auto entity : group) {
                 auto [transform, sprite] = group.get<TransformComponent, SpriteRendererComponent>(entity);
-                //Renderer2D::DrawSprite(transform.GetTransform(), sprite, static_cast<int>(entity));
-                Renderer2D::DrawRect(transform.GetTransform(), glm::vec4(1.0f), static_cast<int>(entity));
+                Renderer2D::DrawSprite(transform.GetTransform(), sprite, static_cast<int>(entity));
+                //Renderer2D::DrawRect(transform.GetTransform(), glm::vec4(1.0f), static_cast<int>(entity));
             }
         }
 
@@ -239,8 +257,8 @@ namespace Hazel {
             }
         }
 
-        Renderer2D::DrawLine(glm::vec3(2.0f), glm::vec3(5.0f), glm::vec4(1, 0, 1, 1));
-        Renderer2D::DrawRect(glm::vec3(0.0f), glm::vec2(1.0f), glm::vec4(1, 1, 1, 1));
+        //Renderer2D::DrawLine(glm::vec3(2.0f), glm::vec3(5.0f), glm::vec4(1, 0, 1, 1));
+        //Renderer2D::DrawRect(glm::vec3(0.0f), glm::vec2(1.0f), glm::vec4(1, 1, 1, 1));
 
         Renderer2D::EndScene();
     }
@@ -269,6 +287,7 @@ namespace Hazel {
         CopyComponentIfExists<NativeScriptComponent>(newEntity, entity);
         CopyComponentIfExists<Rigidbody2DComponent>(newEntity, entity);
         CopyComponentIfExists<BoxCollider2DComponent>(newEntity, entity);
+        CopyComponentIfExists<CircleCollider2DComponent>(newEntity, entity);
     }
 
     Entity Scene::GetPrimaryCameraEntity() {
@@ -320,6 +339,11 @@ namespace Hazel {
 
     template<>
     void Scene::OnComponentAdded<BoxCollider2DComponent>(Entity entity, BoxCollider2DComponent& component) {
+
+    }
+
+    template<>
+    void Scene::OnComponentAdded<CircleCollider2DComponent>(Entity entity, CircleCollider2DComponent& component) {
 
     }
 }
