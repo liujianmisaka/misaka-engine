8bb02295a7ab1bc652e79017b63f6d03d2d5232d
liujian <liujianmisaka@qq.com>
2023-08-30 17:26:29 +0800

    temp

---
+++ Hazel/src/Hazel/Renderer/Renderer3D.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "Hazel/Renderer/Texture.h"
+#include "Hazel/Renderer/Camera.h"
+#include "Hazel/Renderer/OrthographicCamera.h"
+#include "Hazel/Renderer/EditorCamera.h"
+#include "Hazel/Scene/Components.h"
+
+namespace Hazel {
+
+    class Renderer3D {
+    public:
+        static void Init();
+        static void Shutdown();
+
+        static void BeginScene(const Camera& camera, const glm::mat4& transform);
+        static void BeginScene(const EditorCamera& camera);
+        static void BeginScene(const OrthographicCamera& camera); // TODO: Remove
+        static void EndScene();
+        static void Flush();
+
+        // Primitives
+
+        // Stats
+        struct Statistics {
+            uint32_t DrawCalls = 0;
+            uint32_t QuadCount = 0;
+
+            uint32_t GetTotalVertexCount() const { return QuadCount * 4; }
+            uint32_t GetTotalIndexCount() const { return QuadCount * 6; }
+        };
+        static void ResetStats();
+        static Statistics GetStats();
+
+    private:
+        static void StartBatch();
+        static void NextBatch();
+    };
+
+}
